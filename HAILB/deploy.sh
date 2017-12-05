e89db1aff3729f656dd0030376e791f7d8d6f498
Joeri <jvh@jvh.be>
2017-12-05 09:45:54 +0100

    Installing ILB and ELB without backend pool

---
+++ HAILB/deploy.sh
@@ -0,0 +1,72 @@
+#!/bin/sh
+cat << "EOF"
+##############################################################################################################
+#  ____                                      _       
+# | __ )  __ _ _ __ _ __ __ _  ___ _   _  __| | __ _ 
+# |  _ \ / _` | `__| `__/ _` |/ __| | | |/ _` |/ _` |
+# | |_) | (_| | |  | | | (_| | (__| |_| | (_| | (_| |
+# |____/ \__,_|_|  |_|  \__,_|\___|\__,_|\__,_|\__,_|
+#                                                    
+# Script to deploy the Barracuda Next Gen Firewall F HA ILB setup into Microsoft Azure. 
+# This script also creates the network infrastructure needed for it.
+#
+##############################################################################################################
+EOF
+
+# Input location 
+echo "Enter location (e.g. westeurope): "
+stty_orig=`stty -g` # save original terminal setting.
+read location         # read the password
+stty $stty_orig     # restore terminal setting.
+if [ -z "$location" ] 
+then
+    location="westeurope"
+fi
+echo "Deployment in $location location ...\n"
+
+# Input prefix 
+echo "Enter prefix: "
+stty_orig=`stty -g` # save original terminal setting.
+read prefix         # read the prefix
+stty $stty_orig     # restore terminal setting.
+if [ -z "$prefix" ] 
+then
+    prefix="CUDA"
+fi
+echo "Using prefix $prefix for all resources ...\n"
+rg_ngf="$prefix-RG"
+
+# Input password 
+echo "Enter password: "
+stty_orig=`stty -g` # save original terminal setting.
+stty -echo          # turn-off echoing.
+read passwd         # read the password
+stty $stty_orig     # restore terminal setting.
+
+# Create resource group for NextGen Firewall resources
+echo "\nCreating $rg_ngf resource group ...\n"
+az group create --location "$location" --name "$rg_ngf"
+
+# Validate template
+echo "\nValidation deployment in $rg_ngf resource group ...\n"
+az group deployment validate --verbose --resource-group "$rg_ngf" \
+                           --template-file azuredeploy.json \
+                           --parameters rootPassword=$passwd deploymentPrefix=$prefix
+result=$? 
+if [ $result != 0 ]; 
+then 
+    echo "\nValidation failed ...\n"
+    exit $rc; 
+fi
+
+# Deploy NextGen Firewall resources
+echo "\nDeployment of $rg_ngf resources ...\n"
+az group deployment create --resource-group "$rg_ngf" \
+                           --template-file azuredeploy.json \
+                           --parameters rootPassword=$passwd deploymentPrefix=$prefix
+result=$? 
+if [ $result != 0 ]; 
+then 
+    echo "\nDeployment failed ...\n"
+    exit $rc; 
+fi
