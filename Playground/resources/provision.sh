e9691472f1274ed40028b498a651755b8d409227
Joeri <jvh@jvh.be>
2018-10-19 12:16:15 +0200

    Minor typos on the customdata

--- Playground/resources/provision.sh
+++ Playground/resources/provision.sh
@@ -9,11 +9,11 @@ echo "Restore PAR file ... "
 cp /root/config-payg.par /opt/phion/update/box.par && \
     /etc/rc.d/init.d/phion stop && \
     /etc/rc.d/init.d/phion start && \
-    /opb/cloud-setmip $2 $5
+    /opb/cloud-setmip "$2" "$5"
 echo "Restoring PAYG license ... "
 /opb/cloud-restore-license -f
 echo "Creating CGF cluster ... "
-echo $1 | /opb/create-dha -s S1 -c -o "$3" -n "$4" -g "$5"
+echo "$1" | /opb/create-dha -s S1 -c -o "$3" -n "$4" -g "$5"
 } > /tmp/provision.log 2>&1
 
 #    "ngfCustomData1": "[base64(concat('#!/bin/bash\n\n', '{ echo \"Starting Barracuda CloudGen Firewall bootstrap.\"\necho \"nameserver 168.63.129.16\" > /etc/resolv.conf\ncurl \"https://raw.githubusercontent.com/jvhoof/quickstart-blue-green-azure/master/resources/quickstart-blue.par\" --output /root/backupdeployment.par\ncp /root/backupdeployment.par /opt/phion/update/box.par && /etc/rc.d/init.d/phion stop && /etc/rc.d/init.d/phion start && /opb/cloud-setmip', variables('ngfVmAddress1'), variables( 'ngfSubnetMask' ), variables( 'ngfSubnetDefaultGw' ), '\n/opb/cloud-restore-license -f\n} > /tmp/provision.log\n'))]",
