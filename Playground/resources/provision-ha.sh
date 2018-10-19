48413aacb1b17a3b1229d8fcef3968edabf155f1
Joeri <jvh@jvh.be>
2018-10-19 16:04:27 +0200

    Updated provisioning scripts

--- Playground/resources/provision-ha.sh
+++ Playground/resources/provision-ha.sh
@@ -4,12 +4,12 @@ echo "Starting Barracuda CloudGen Firewall bootstrap script"
 echo "Setting DNS server ... "
 echo "nameserver 168.63.129.16" > /etc/resolv.conf
 echo "Download PAR file ... "
-curl https://raw.githubusercontent.com/jvhoof/ngf-azure-templates/master/Playground/resources/config-payg-ha.par --output /root/config-payg.par
+curl https://raw.githubusercontent.com/jvhoof/ngf-azure-templates/master/Playground/resources/config-payg-ha.par --output /root/config-payg-ha.par
 echo "Restore PAR file ... "
 cp /root/config-payg-ha.par /opt/phion/update/box.par && \
     /etc/rc.d/init.d/phion stop && \
     /etc/rc.d/init.d/phion start && \
-    /opb/cloud-setmip "$2" "$4" "$5"
+    /opb/cloud-setmip "$1" "$2" "$3"
 echo "Restoring PAYG license ... "
 /opb/cloud-restore-license -f
-} > /tmp/provision.log 2>&1
+} > /tmp/provision-ha.log 2>&1
