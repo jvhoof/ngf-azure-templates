dd33a0ddf80f370fa92d493cf62f44d8badede7e
Joeri <jvh@jvh.be>
2018-10-22 20:58:42 +0200

    Remove par restore

--- Playground/resources/provision.sh
+++ Playground/resources/provision.sh
@@ -9,11 +9,11 @@ echo "Setting DNS server ... "
 echo "nameserver 168.63.129.16" > /etc/resolv.conf
 echo "Download PAR file ... "
 curl https://raw.githubusercontent.com/jvhoof/ngf-azure-templates/master/Playground/resources/config-payg.par --output /root/config-payg.par
-echo "Restore PAR file ... "
-cp /root/config-payg.par /opt/phion/update/box.par && \
-    /etc/rc.d/init.d/phion stop && \
-    /etc/rc.d/init.d/phion start && \
-    /opb/cloud-setmip "$2" "$4" "$5"
+#echo "Restore PAR file ... "
+#cp /root/config-payg.par /opt/phion/update/box.par && \
+#    /etc/rc.d/init.d/phion stop && \
+#    /etc/rc.d/init.d/phion start && \
+#    /opb/cloud-setmip "$2" "$4" "$5"
 echo "Restoring PAYG license ... "
 /opb/cloud-restore-license -f
 #echo "Creating CGF cluster after restore ... "
